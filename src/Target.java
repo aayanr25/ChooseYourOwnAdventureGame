dd5507e805a5b247b7f8bb0e565fbcaae6279742
Aayan Ramamurthy <aayan.ramamurthy@gmail.com>
2023-12-04 19:01:05 -0500

    most classes added, but not completed

--- src/Target.java
+++ src/Target.java
@@ -2,14 +2,10 @@ public class Target {
     private String name;
     private int stat;
     private int health = 50;
-<<<<<<< HEAD
-
-=======
     private Target barthy = new Target("Barthy", 5);
     private Target michael = new Target("Michael A.", 10);
     private Target piper =
     private Target jenna = new Target("Jenna", 20);
->>>>>>> origin/master
     public Target(String name, int stat) {
         this.name = name;
         this.stat = stat;
